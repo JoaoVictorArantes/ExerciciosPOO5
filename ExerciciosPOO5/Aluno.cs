c7466615bed629a6ccc1fe791bd36d1a2b8be12f
JoaoVictorArantes <jvaarantes@gmail.com>
2024-01-25 21:05:29 -0300

    Add project files.

---
+++ ExerciciosPOO5/Aluno.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosPOO5
+{
+    internal class Aluno
+    {
+        public String? Nome;
+        public double nota1;
+        public double nota2;
+        public double nota3;
+
+        public double PontosTotal()
+        {
+            return nota1 + nota2 + nota3;
+        }
+        public bool Aprovado()
+        {
+            if (PontosTotal() >= 60)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public String Situacao()
+        {
+            if (Aprovado())
+            {
+                return "Aprovado! :)";
+            }
+            else
+            {
+                return $"Reprovado! :( \nFaltaram {60 - PontosTotal()} pontos. :(";
+            }
+        }             
+        public override string ToString()
+        {
+            return $"Nota Final = {PontosTotal().ToString("F2")}";
+        }
+    }
+}
---
+++ ExerciciosPOO5/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ExerciciosPOO5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Aluno aluno = new Aluno();
+
+            Console.WriteLine("Digite os dados do aluno:");
+            Console.Write("Nome do Aluno: ");
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno:");
+            aluno.nota1 = Convert.ToDouble(Console.ReadLine());
+            aluno.nota2 = Convert.ToDouble(Console.ReadLine());
+            aluno.nota3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(aluno);
+            Console.WriteLine(aluno.Situacao());
+
+            Console.ReadKey();
+        }
+    }
+}
